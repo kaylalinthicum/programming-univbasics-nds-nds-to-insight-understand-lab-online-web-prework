f9d657dd620e6f8a5dff2de22107fc56743f864a
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-27 01:47:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,8 +9,6 @@ end
 pretty_print_nds(directors_database)
 
 def print_first_directors_movie_titles
-  row_index = 0 
-  while row_index < directors_database.count do
     directors_database.first[movie]
     
 end
